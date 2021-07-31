f06032b783bffcbd3725254ebc28bef64b063d7e
jaroldhakins <stivenhakins@gmail.com>
2021-07-31 00:47:40 +0000

    0-display.c

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -3,23 +3,40 @@
 /**
  * display_complex_number - prints complex numbers
  *
- * @c: complex
+ * @c: structur  complex
  *
  * Return: no return
  **/
 void display_complex_number(complex c)
 {
-	if (c.im == 0)
-	{
-		printf("%.f\n", c.re);
-	}
-	else if (c.im <= 1)
-	{
-		c.im = c.im * -1;
-		printf("%.f - %fi\n", c.re, c.im);
-	}
-	else
-	{
-		printf("%.f + %.fi\n", c.re, c.im);
-	}
+		if (c.im > 1)
+		{
+			printf("%.0f + %.0fi\n", c.re, c.im);
+			
+		}
+		if (c.im == 0)
+		{
+			printf("%.0f\n", c.re);
+			
+		}
+		if (c.re == 0)
+		{
+			printf("%.0fi\n", c.im);
+			
+		}
+		if (c.im == 1)
+		{
+			printf("%.0f + i\n", c.re);
+			
+		}
+		if (c.im == -1)
+		{
+			printf("%.0f - i\n", c.re);
+			
+		}
+		if(c.im < -1)
+		{
+			printf("%.0f - %.0fi\n", c.re, (-c.im));
+			
+		}
 }
